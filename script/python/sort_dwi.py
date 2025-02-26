0fa165aba1e50c0dde2be2aaefe2d35a6e4c79d8
aldoclemente <aldo.clemente@polimi.it>
2025-02-26 17:06:55 +0100

    DWI data are filtered!

--- script/python/sort_dwi.py
+++ script/python/sort_dwi.py
@@ -1,6 +1,7 @@
 import numpy as np
 import nibabel as nib
 import os
+from collections import Counter
 from pathlib import Path
 
 import argparse
@@ -25,10 +26,6 @@ sorted_indices = np.argsort(bvals)
 bvals_sorted = bvals[sorted_indices]
 bvecs_sorted = bvecs[:, sorted_indices]
 
-# Save reordered gradient files
-np.savetxt(outdir + "bvals", bvals_sorted, fmt="%d")
-np.savetxt(outdir + "bvecs", bvecs_sorted, fmt="%.6f")
-
 # Load NIfTI file and reorder volumes
 img = nib.load(args.data)
 data = img.get_fdata()
@@ -37,9 +34,30 @@ affine = img.affine
 # Reorder diffusion volumes
 data_sorted = data[..., sorted_indices]
 
+# FILTER
+# Set minimum number of volumes per shell
+MIN_SHELL_SIZE = 10  # Adjust this threshold as needed
+
+# Count occurrences of each b-value
+bval_counts = Counter(bvals_sorted)
+
+# Identify shells to keep
+valid_shells = {b for b, count in bval_counts.items() if count >= MIN_SHELL_SIZE}
+
+# Get indices of volumes to keep
+keep_indices = np.array([i for i, b in enumerate(bvals_sorted) if b in valid_shells])
+
+# store...
+bvals_filtered = bvals_sorted[keep_indices]
+bvecs_filtered = bvecs_sorted[:, keep_indices]
+
+np.savetxt(outdir + "bvals", bvals_filtered, fmt="%d")
+np.savetxt(outdir + "bvecs", bvecs_filtered, fmt="%.6f")
+
 # Save reordered NIfTI file
-new_img = nib.Nifti1Image(data_sorted, affine)
+data_filtered = data_sorted[..., keep_indices]
+new_img = nib.Nifti1Image(data_filtered, affine)
 nib.save(new_img, outdir + "data.nii.gz")
 
-print("Reordering complete!")
+print("Reordering & filtering complete!")
 
