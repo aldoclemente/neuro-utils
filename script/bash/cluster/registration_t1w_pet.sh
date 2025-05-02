918e37dc5707b03f93f6939017d2af877e2746a6
aldoclemente <aldo.clemente@polimi.it>
2025-05-02 16:33:15 +0200

    registration to MNI152 using cluster.

---
+++ script/bash/cluster/registration_t1w_pet.sh
@@ -0,0 +1,43 @@
+#!/bin/bash 
+
+#PBS -S /bin/bash
+#PBS -l nodes=1:ppn=96,walltime=12:00:00
+#PBS -j oe
+
+# $1 subject ID
+
+cd $1
+
+T1DIR="T1w"
+PETDIR="PET"
+DTIDIR="DTI" 
+UTILSDIR=$HOME/neuro-utils
+FSL=$HOME/fsl_latest.sif
+APPTAINER=/opt/mox/apptainer/bin/apptainer
+MNI152T1=$UTILSDIR/data/mni_icbm152_nlin_asym_09c/nifti/mni_icbm152_t1_tal_nlin_asym_09c.nii
+MNI152BRAIN=$UTILSDIR/data/mni_icbm152_nlin_asym_09c/nifti/mni_icbm152_t1_tal_nlin_asym_09c_brain.nii.gz
+
+# T1w registration
+cd $T1DIR
+for k in */; do
+    cd $k
+    $APPTAINER exec $FSL bet unwarped.nii.gz unwarped_bet.nii.gz -B -f 0.15 # -B Bias Field & Neck cleanup (necessario)
+	$APPTAINER exec $FSL flirt -ref $MNI152BRAIN -in unwarped_bet.nii.gz -omat affine_transf.mat
+	$APPTAINER exec $FSL fnirt --in=unwarped.nii.gz --aff=affine_transf.mat --cout=nonlinear_transf --config=$UTILSDIR/data/mni_icbm152_nlin_asym_09c/config
+	$APPTAINER exec $FSL applywarp --ref=$MNI152T1 \
+		  --in=unwarped.nii.gz --warp=nonlinear_transf --out=warped.nii.gz
+$APPTAINER exec $FSL slice warped.nii.gz $MNI152T1 -o slices.png
+    cd ..
+done
+cd .. # sub
+
+# PET registration
+cd $PET
+for k in */; do
+    cd $k
+    $APPTAINER exec $FSL flirt -ref ../../$T1DIR/$k/unwarped_bet.nii.gz -in unwarped.nii.gz -dof 6 -omat func2struct.mat
+    $APPTAINER exec $FSL applywarp --ref=$MNI152T1 --in=unwarped.nii.gz --warp=../../$T1DIR/$k/nonlinear_transf --premat=func2struct.mat --out=warped.nii.gz
+    cd ..
+done
+cd ..
+	
