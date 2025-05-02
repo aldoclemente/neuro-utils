d86ca9af1d73a059345b61edd9933f74d9ca947e
aldoclemente <aldo.clemente@polimi.it>
2025-05-02 11:49:37 +0200

    minor changes.

--- script/bash/adni_2_arrange_data.sh
+++ script/bash/adni_2_arrange_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ADNI
+cd $1
 
 T1DIR="T1w"
 T1RAWDIR="Accelerated_Sagittal_MPRAGE"
@@ -11,26 +11,17 @@ PETRAWDIR="AV1451_Coreg,_Avg,_Std_Img_and_Vox_Siz,_Uniform_Resolution"
 DTIDIR="DTI" 
 DTIRAWDIR1="Axial_DTI"
 DTIRAWDIR2="Axial_MB_DTI"
-#RAWDIR="raw"
 
-for sub in `ls`; do
+DIRS=($T1DIR $PETDIR $DTIDIR)
+
+for sub in *; do
 	cd $sub
-	
-	#if ! [ -d $RAWDIR ]; then
-	#	mkdir $RAWDIR
-	#fi
-	
-	if ! [ -d $T1DIR ]; then
-		mkdir $T1DIR
-	fi 
-	
-	if ! [ -d $PETDIR ]; then
-		mkdir $PETDIR
-	fi 
-
-	if ! [ -d $DTIDIR ]; then
-		mkdir $DTIDIR
-	fi 
+		
+    for DIR in ${DIRS[@]}; do
+        if ! [ -d $DIR ]; then
+		    mkdir $DIR
+	    fi
+	done 
 	
 	if [ -d $T1RAWDIR ]; then
 		mv $T1RAWDIR/* $T1DIR/
@@ -50,8 +41,19 @@ for sub in `ls`; do
 	if [ -d $DTIRAWDIR2 ]; then
 		mv $DTIRAWDIR2/* $DTIDIR/
 		rm -r $DTIRAWDIR2
-	fi 
+	fi
 	
+	# get rid of dates
+	for DIR in ${DIRS[@]}; do
+	k=0
+	cd $DIR
+	    for date in */; do
+		mv $date $k
+	    k=$(($k+1))
+    	done 	
 	cd ..
+	done
+	
+	cd .. 
 done
 
