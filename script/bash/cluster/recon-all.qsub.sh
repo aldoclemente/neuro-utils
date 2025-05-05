409292f969d021ca8ed357932a4ebcffaf0b7241
aldoclemente <aldo.clemente@polimi.it>
2025-05-05 22:12:46 +0200

    Added MNI152 1mm template available with FSL.

--- script/bash/cluster/recon-all.qsub.sh
+++ script/bash/cluster/recon-all.qsub.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 #PBS -S /bin/bash
-#PBS -l nodes=1:ppn=96
-#PBS -l walltime=48:00:00
-#PBS -e ~/error.txt
-#PBS -o ~/output.txt
+#PBS -l nodes=1:ppn=96,walltime=48:00:00
 #PBS -N recon-all
 
 SUBJECTS_DIR=$HOME/.freesurfer/subjects/
+#ID=mni_icbm152_nlin_asym_09c
+#DATA=$HOME/neuro-utils/data/$ID/nifti
+#T1=$DATA/mni_icbm152_t1_tal_nlin_asym_09c.nii
+#T2=$DATA/mni_icbm152_t2_tal_nlin_asym_09c.nii
 
-ID=mni_icbm152_nlin_asym_09c
+ID=fsl_mni152_1mm
 DATA=$HOME/neuro-utils/data/$ID/nifti
-T1=$DATA/mni_icbm152_t1_tal_nlin_asym_09c.nii
-T2=$DATA/mni_icbm152_t2_tal_nlin_asym_09c.nii
+T1=$DATA/MNI152_T1_1mm.nii.gz
 
 APPTAINER=/opt/mox/apptainer/bin/apptainer
 IMG=$HOME/freesurfer_7.4.1.sif
@@ -21,5 +21,7 @@ LICENSE=$HOME/.freesurfer/license.txt
 
 if [ -d $SUBJECTS_DIR/$ID ]; then rm -r $SUBJECTS_DIR/$ID; fi
     
-$APPTAINER exec -B $LICENSE:/usr/local/freesurfer/license.txt -B $SUBJECTS_DIR:/output --env SUBJECTS_DIR=/output/ $IMG recon-all -s $ID -i $T1 -T2 $T2 -all
-
+#$APPTAINER exec -B $LICENSE:/usr/local/freesurfer/license.txt -B $SUBJECTS_DIR:/output --env SUBJECTS_DIR=/output/ $IMG recon-all -s $ID -i $T1 -T2 $T2 -all
+START=$(date +%s)
+$APPTAINER exec -B $LICENSE:/usr/local/freesurfer/license.txt -B $SUBJECTS_DIR:/output --env SUBJECTS_DIR=/output/ $IMG recon-all -s $ID -i $T1 -all
+echo execution time $(( $(date +%s) - $START )) secs
