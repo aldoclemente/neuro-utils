e120f1fa58fbd6bbfd35f591c4f5251381d63daf
aldoclemente <aldo.clemente@polimi.it>
2025-05-09 07:19:33 +0200

    minor changes.

--- script/bash/cluster/run.registration_t1w_pet.sh
+++ script/bash/cluster/run.registration_t1w_pet.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-cd $HOME/ADNI3-cohort
-INPUTFILE=$HOME/ADNI3-cohort/$inputfile
-
-mkdir -p logs
+adni3data=$HOME/ADNI3-cohort
+inputfile=$adni3data/$1
+scripts=$HOME/neuro-utils(script/bash/cluster
+logs=$adni3data/logs
+mkdir -p $logs
 
 while read -r subject; do
-qsub -N reg-$subject -v subj=$subject -o logs/out.reg-MNI152-$subject.txt -e logs/err.reg-MNI152-$subject.txt registration_t1w_pet.sh 
+qsub -N reg-$subject -v subj=$subject -o $logs/out.reg-MNI152-$subject.txt -e $logs/err.reg-MNI152-$subject.txt $scripts/registration_t1w_pet.sh 
 done < $inputfile
 
 
