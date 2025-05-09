8916df73dee97e4cfd1f77102ed11695acb57164
aldoclemente <aldo.clemente@polimi.it>
2025-05-09 07:23:29 +0200

    Fixing...

--- script/bash/cluster/run.registration_t1w_pet.sh
+++ script/bash/cluster/run.registration_t1w_pet.sh
@@ -2,12 +2,12 @@
 
 adni3data=$HOME/ADNI3-cohort
 inputfile=$adni3data/$1
-scripts=$HOME/neuro-utils(script/bash/cluster
+scripts=$HOME/neuro-utils/script/bash/cluster
 logs=$adni3data/logs
 mkdir -p $logs
 
-while read -r subject; do
-qsub -N reg-$subject -v subj=$subject -o $logs/out.reg-MNI152-$subject.txt -e $logs/err.reg-MNI152-$subject.txt $scripts/registration_t1w_pet.sh 
+while read -r subj; do
+qsub -N reg-$subj -v subject=$subj -o $logs/out.reg-MNI152-$subj.txt -e $logs/err.reg-MNI152-$subj.txt $scripts/registration_t1w_pet.sh 
 done < $inputfile
 
 
